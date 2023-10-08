ce8da441f4c7829539ad1e9a4c202f7784d615a1
Sophia <sophiamperez@hotmail.com>
2023-10-08 16:34:20 -0300

    ajuste na controllerONG

--- API/Controllers/ONGController.cs
+++ API/Controllers/ONGController.cs
@@ -4,195 +4,131 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
-namespace API
+namespace API;
+
+[ApiController]
+[Route("api/ong")]
+public class ONGController : ControllerBase
 {
-    [ApiController]
-    [Route("api/ong")]
-    public class ONGController : ControllerBase
+    private readonly AppDataContext _ctx;
+
+    public ONGController(AppDataContext ctx)
     {
-        private readonly AppDataContext _ctx;
+        _ctx = ctx;
+    }
+    private static List<ONG> ongs = new List<ONG>();
 
-        //GET: api/ong/listar
-        [HttpGet]
-        [Route("listar")]
-        public IActionResult Listar()
+    //GET: api/ong/listar
+    [HttpGet]
+    [Route("listar")]
+    public IActionResult Listar()
+    {
+        try
         {
-            try
-            {
-                List<ONG> ongs = _ctx.ONGs.ToList();
-                return Ok(ongs);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
+            List<ONG> ongs = _ctx.ONGs.ToList();
+            return Ok(ongs);
         }
-        //GET: api/ong/listar
-        [HttpGet]
-        [Route("listar")]
-
-        //GET: api/ong/buscar/{bolacha}
-        [HttpGet]
-        [Route("buscar/{nome}")]
-        public IActionResult Buscar([FromRoute] string nome)
+        catch (Exception e)
         {
-            try
+            return BadRequest(e.Message);
+        }
+    }
+
+    //GET: api/ong/buscar/{bolacha}
+    [HttpGet]
+    [Route("buscar/{nome}")]
+    public IActionResult Buscar([FromRoute] string nome)
+    {
+        try
+        {
+            ONG? ongCadastrada =
+                _ctx.ONGs
+                .FirstOrDefault(x => x.Nome == nome);
+            if (ongCadastrada != null)
             {
-                ONG? ongCadastrada =
-                    _ctx.ONGs
-                    .FirstOrDefault(x => x.Nome == nome);
-                if (ongCadastrada != null)
-                {
-                    return Ok(ongCadastrada);
-                }
+                return Ok(ongCadastrada);
+            }
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    //POST: api/ong/cadastrar
+    [HttpPost]
+    [Route("cadastrar")]
+    public IActionResult Cadastrar([FromBody] ONG ong)
+    {
+        try
+        {
+            _ctx.ONGs.Add(ong);
+            _ctx.SaveChanges();
+            return Created("", ong);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
+
+    // PUT: api/ong/atualizar/{id}
+    [HttpPut]
+    [Route("atualizar/{id}")]
+    public IActionResult Atualizar([FromRoute] int id, [FromBody] ONG ongAtualizada)
+    {
+        try
+        {
+            ONG ongExistente = _ctx.ONGs.Find(id);
+
+            if (ongExistente == null)
+            {
                 return NotFound();
             }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
 
-        //POST: api/ong/cadastrar
-        [HttpPost]
-        [Route("cadastrar")]
-        public IActionResult Cadastrar([FromBody] ONG ong)
+            // Atualizar as propriedades da ongExistente com os valores da ongAtualizada
+
+            ongExistente.Nome = ongAtualizada.Nome;
+            ongExistente.Missao = ongAtualizada.Missao;
+            ongExistente.Historico = ongAtualizada.Historico;
+            ongExistente.InformacoesContato = ongAtualizada.InformacoesContato;
+
+
+            // Adicione outras propriedades que deseja atualizar
+
+            _ctx.SaveChanges();
+
+            return Ok(ongExistente);
+        }
+        catch (Exception e)
         {
-            try
+            return BadRequest(e.Message);
+        }
+    }
+
+
+
+
+    [HttpDelete]
+    [Route("delete/{id}")]
+    public IActionResult Deletar([FromRoute] int id)
+    {
+        try
+        {
+            ONG? ongCadastrada = _ctx.ONGs.Find(id);
+            if (ongCadastrada != null)
             {
-                _ctx.ONGs.Add(ong);
+                _ctx.ONGs.Remove(ongCadastrada);
                 _ctx.SaveChanges();
-                return Created("", ong);
+                return Ok();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return BadRequest(e.Message);
-            }
+            return NotFound();
         }
-
-        // PUT: api/ong/atualizar/{id}
-        [HttpPut]
-        [Route("atualizar/{id}")]
-        public IActionResult Atualizar([FromRoute] int id, [FromBody] ONG ongAtualizada)
+        catch (Exception e)
         {
-            try
-            {
-                ONG ongExistente = _ctx.ONGs.Find(id);
-
-                if (ongExistente == null)
-                {
-                    return NotFound();
-                }
-
-                // Atualizar as propriedades da ongExistente com os valores da ongAtualizada
-
-                ongExistente.Nome = ongAtualizada.Nome;
-                ongExistente.Missao = ongAtualizada.Missao;
-                ongExistente.Historico = ongAtualizada.Historico;
-                ongExistente.InformacoesContato = ongAtualizada.InformacoesContato;
-
-
-                // Adicione outras propriedades que deseja atualizar
-
-                _ctx.SaveChanges();
-
-                return Ok(ongExistente);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
+            return BadRequest(e.Message);
         }
-
-
-
-
-        [HttpDelete]
-        [Route("deletar/{id}")]
-        public IActionResult Deletar([FromRoute] int id)
-        {
-            try
-            {
-                ONG? ongCadastrada = _ctx.ONGs.Find(id);
-                if (ongCadastrada != null)
-                {
-                    _ctx.ONGs.Remove(ongCadastrada);
-                    _ctx.SaveChanges();
-                    return Ok();
-                }
-                return NotFound();
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-        // PUT: api/ong/atualizar/{id}
-        [HttpPut]
-        [Route("atualizar/{id}")]
-        public IActionResult Atualizar([FromRoute] int id, [FromBody] ONG ongAtualizada)
-        {
-            try
-            {
-                ONG ongExistente = _ctx.ONGs.Find(id);
-
-                if (ongExistente == null)
-                {
-                    return NotFound();
-                }
-
-                // Atualizar as propriedades da ongExistente com os valores da ongAtualizada
-
-                ongExistente.Nome = ongAtualizada.Nome;
-                ongExistente.Missao = ongAtualizada.Missao;
-                ongExistente.Historico = ongAtualizada.Historico;
-                ongExistente.InformacoesContato = ongAtualizada.InformacoesContato;
-
-
-                // Adicione outras propriedades que deseja atualizar
-
-                _ctx.SaveChanges();
-
-                return Ok(ongExistente);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-
-
-
-        [HttpDelete]
-        [Route("delete/{id}")]
-        public IActionResult Deletar([FromRoute] int id)
-        {
-            try
-            {
-                try
-                {
-                    ONG? ongExistente = _ctx.ONGs.Find(id);
-
-                    if (ongExistente == null)
-                    {
-                        return NotFound();
-                    }
-
-                    ongExistente.Nome = ongAtualizado.Nome;
-                    ongExistente.Missao = ongAtualizado.Missao;
-                    ongExistente.Historico = ongAtualizado.Historico;
-                    ongExistente.InformacoesContato = ongAtualizado.InformacoesContato;
-
-                    // Salve as alterações no banco de dados
-                    _ctx.SaveChanges();
-
-                    return Ok(ongExistente);
-                }
-                catch (Exception e)
-                {
-                    return BadRequest(e.Message);
-                }
-            }}
+    }
+}
